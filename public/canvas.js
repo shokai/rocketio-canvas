e072cc2ccc0741940b7afe56bd668c4f009cea8c
Sho Hashimoto <hashimoto@shokai.org>
2013-04-11 18:56:04 +0900

    touchmove event for tablet #1

--- public/canvas.js
+++ public/canvas.js
@@ -13,22 +13,38 @@ var Canvas = function(opts){
   this.target.attr("width", this.width).attr("height", this.height);
   var ctx = this.target[0].getContext("2d");
   this.is_drawing = false;
-  $("body").mouseup(function(){
+  var last_pos = null;
+  $("body").bind("mouseup", function(){
     self.is_drawing = false;
+    last_pos = null;
   });
-  this.target.mousedown(function(){
+  this.target.bind("mousedown", function(){
     self.is_drawing = true;
   });
-  var last_pos = {x: null, y: null};
-  var cursor_move = function(e){
+  this.target.bind("touchend", function(){
+    self.is_drawing = false;
+    last_pos = null;
+  });
+  this.target.bind("touchstart", function(){
+    self.is_drawing = true;
+  });
+  this.target.bind("mousemove", function(e){
     var rect = e.target.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
     var pos = {x: x, y: y};
-    if(self.is_drawing) self.emit("draw", {from: last_pos, to: pos});
+    if(self.is_drawing && last_pos !== null) self.emit("draw", {from: last_pos, to: pos});
     last_pos = pos;
-  };
-  this.target.mousemove(cursor_move);
+  });
+  this.target.bind("touchmove", function(e){
+    e.preventDefault();
+    var rect = e.target.getBoundingClientRect();
+    var x = event.changedTouches[0].pageX - rect.left;
+    var y = event.changedTouches[0].pageY - rect.top;
+    var pos = {x: x, y: y};
+    if(self.is_drawing && last_pos !== null) self.emit("draw", {from: last_pos, to: pos});
+    last_pos = pos;
+  });
   this.draw_line = function(line){
     ctx.strokeStyle = line.style;
     ctx.lineWidth = line.width;
