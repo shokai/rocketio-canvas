e030021eeb84eaff0f35cd3774cf5ae792ae8ed6
Sho Hashimoto <hashimoto@shokai.org>
2013-04-11 10:55:11 +0900

    save logs

--- main.rb
+++ main.rb
@@ -1,11 +1,19 @@
 io = Sinatra::RocketIO
+logs = Hash.new{|h,k| h[k] = Array.new }
 
 io.on :connect do |client|
   puts "new #{client.type} connect!! <#{client.session}>"
+  logs[client.channel].each do |log|
+    io.push :draw, log, :to => client.session
+  end
 end
 
 io.on :draw do |data, client|
   puts "draw : #{data} from <#{client}>"
+  logs[client.channel].push data
+  while logs[client.channel].size > 10000
+    logs[client.channel].shift
+  end
   io.push :draw, data, :channel => client.channel
 end
 
